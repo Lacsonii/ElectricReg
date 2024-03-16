0bf5d96ddc86aa6e29b68f762ce5c8243290e152
Daniel P. Mgawi <pdmgawi@gmail.com>
2024-03-16 14:39:37 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace ElectricReg
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Home());
+            Application.Run(new SignIn());
         }
     }
 }
