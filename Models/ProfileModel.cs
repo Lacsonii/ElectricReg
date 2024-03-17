6d4ad7fc2a69766c33f82b300fd426af0d4322e3
Encrypt-lac <152815763+Encrypt-lac@users.noreply.github.com>
2024-03-17 13:27:51 +0200

    Create ProfileModel.cs
    Added ProfileModel

---
+++ Models/ProfileModel.cs
@@ -0,0 +1,26 @@
+﻿using Postgrest.Attributes;
+using Postgrest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElectricReg.Models
+{
+    [Table("profile")]
+     class ProfileModel : BaseModel
+    {
+        [PrimaryKey("id", false)]
+        public string Id { get; set; }
+
+        [Column("student_id")]
+        public string StudentID { get; set; }
+
+        [Column("student_name")]
+        public string StudnetName { get; set; }
+
+        [Column("avatar_url")]
+        public string AvatarUrl { get; set; }
+    }
+}
